b98b480e372aa01e95ed0a7fdf9dc346607ca34a
Maciej Sienkiewicz <maciej@niczyja.pl>
2024-02-05 23:54:04 +0100

    add empty project

---
+++ WawTreesStats/WawTreesStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WawTreesStatsApp.swift
+//  WawTreesStats
+//
+//  Created by Maciej Sienkiewicz on 05/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct WawTreesStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
